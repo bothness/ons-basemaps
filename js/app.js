040d52eac92d059bad2f052d6210a4ac2601ec61
bothness <ark@visualizingimpact.org>
2020-12-18 13:47:31 +0200

    added historic maps and overlays

---
+++ js/app.js
@@ -0,0 +1,115 @@
+var mapLeft = new mapboxgl.Map({
+  container: 'before',
+  style: styles[0].url,
+  center: [-1.2471735, 50.8625412],
+  zoom: 13
+});
+
+var mapRight = new mapboxgl.Map({
+  container: 'after',
+  style: styles[0].url,
+  center: [-1.2471735, 50.8625412],
+  zoom: 13
+});
+
+// A selector or reference to HTML element
+var container = '#comparison-container';
+
+var map = new mapboxgl.Compare(mapLeft, mapRight, container, {
+  // Set this to enable comparing two maps by mouse movement:
+  // mousemove: true
+});
+
+// Function to get layer url by ID
+function getLayer(id) {
+  let obj = styles.find(obj => obj.id == id);
+  return obj.url;
+}
+
+// Function to generate map options
+function genOptions(side) {
+  let html = '';
+  for (style in styles) {
+    let checked = style == 0 ? ' checked="checked"' : '';
+    html += '<input value="' + styles[style].id + '" type="radio" name="' + side + '"' + checked + ' /><label for="' + styles[style].id + '">' + styles[style].name + '</label><br />';
+  }
+  for (overlay in overlays) {
+    html += '<hr/><input value="' + overlays[overlay].id + '" type="checkbox" name="' + side + '-overlays" /><label for="' + overlays[overlay].id + '">' + overlays[overlay].name + '</label><br />';
+  }
+  document.getElementById(side).innerHTML = html;
+}
+
+genOptions('menu-left');
+genOptions('menu-right');
+
+var inputsLeft = document.getElementsByName('menu-left');
+var inputsRight = document.getElementsByName('menu-right');
+
+var overlaysLeft = document.getElementsByName('menu-left-overlays');
+var overlaysRight = document.getElementsByName('menu-right-overlays');
+
+// Switch layer function
+function switchLayer(value, map) {
+  let url = getLayer(value);
+  map.setStyle(url);
+  refreshOverlays(map);
+}
+
+// Switch overlays function
+function switchOverlay(input, map) {
+  let overlay = overlays.filter(d => d.id == input.value)[0];
+  let id = overlay.id;
+  let url = overlay.url;
+  let layers = overlay.layers;
+  if (input.checked) {
+    if (!map.getSource(id)) {
+      map.addSource(id, {
+        'type': 'vector',
+        'url': url
+      });
+    }
+    for (layer in layers) {
+      map.addLayer(layers[layer]);
+    }
+  } else {
+    for (layer in layers) {
+      map.removeLayer(layers[layer].id);
+    }
+  }
+}
+
+function refreshOverlays(map) {
+  map.on('style.load', () => {
+    if (map == mapLeft) {
+      for (i in overlaysLeft) {
+        if (overlaysLeft[i].checked) {
+          switchOverlay(overlaysLeft[i], map);
+        }
+      }
+    } else if (map == mapRight) {
+      for (i in overlaysRight) {
+        if (overlaysRight[i].checked) {
+          switchOverlay(overlaysRight[i], map);
+        }
+      }
+    }
+  });
+}
+
+// Add event listeners
+for (var i = 0; i < inputsLeft.length; i++) {
+  let value = inputsLeft[i].value;
+  inputsLeft[i].onclick = () => { switchLayer(value, mapLeft) };
+}
+for (var i = 0; i < inputsRight.length; i++) {
+  let value = inputsRight[i].value;
+  inputsRight[i].onclick = () => { switchLayer(value, mapRight) };
+}
+for (var i = 0; i < overlaysLeft.length; i++) {
+  let value = overlaysLeft[i].value;
+  overlaysLeft[i].onclick = (e) => { switchOverlay(e.target, mapLeft) };
+}
+for (var i = 0; i < overlaysRight.length; i++) {
+  let value = overlaysRight[i].value;
+  overlaysRight[i].onclick = (e) => { switchOverlay(e.target, mapRight) };
+}
---
+++ js/config.js
@@ -0,0 +1,138 @@
+const styles = [
+  {
+    'id': 'ons-light',
+    'name': 'OpenMapTiles (ONS style)',
+    'url': './data/style-omt.json'
+  },
+  {
+    'id': 'ons-dark',
+    'name': 'OpenMapTiles (ONS dark)',
+    'url': './data/style-dark.json'
+  },
+  {
+    'id': 'zoomstack-ons',
+    'name': 'OS Zoomstack (ONS style)',
+    'url': './data/style-os.json'
+  },
+  {
+    'id': 'zoomstack-outdoor',
+    'name': 'OS Zoomstack (outdoor style)',
+    'url': './data/style-outdoor.json'
+  },
+  {
+    'id': 'os-1900',
+    'name': 'Ordnance Survey 1900s',
+    'url': {
+      'version': 8,
+      'sources': {
+        'os-1900': {
+          'type': 'raster',
+          'tiles': [
+            'https://nls-1.tileserver.com/fpsUZbqQLWLT/{z}/{x}/{y}.png'
+          ],
+          'tileSize': 256,
+          'attribution': '<a href="https://maps.nls.uk/geo/explore/side-by-side/" target="_blank">Ordnance Survey / National Library of Scotland</a>',
+          'maxzoom': 17
+        }
+      },
+      'layers': [
+        {
+          'id': 'os-1900',
+          'type': 'raster',
+          'source': 'os-1900'
+        }
+      ],
+      "sprite": "https://openmaptiles.github.io/positron-gl-style/sprite",
+      "glyphs": "https://cdn.ons.gov.uk/maptiles/osm/fontsv1/{fontstack}/{range}.pbf",
+    }
+  },
+  {
+    'id': 'os-1937',
+    'name': 'Ordnance Survey 1937-61',
+    'url': {
+      'version': 8,
+      'sources': {
+        'os-1937': {
+          'type': 'raster',
+          'tiles': [
+            'https://nls-1.tileserver.com/fpsUZbIoj0Oa/{z}/{x}/{y}.png'
+          ],
+          'tileSize': 256,
+          'attribution': '<a href="https://maps.nls.uk/geo/explore/side-by-side/" target="_blank">Ordnance Survey / National Library of Scotland</a>',
+          'maxzoom': 15
+        }
+      },
+      'layers': [
+        {
+          'id': 'os-1937',
+          'type': 'raster',
+          'source': 'os-1937'
+        }
+      ],
+      "sprite": "https://openmaptiles.github.io/positron-gl-style/sprite",
+      "glyphs": "https://cdn.ons.gov.uk/maptiles/osm/fontsv1/{fontstack}/{range}.pbf",
+    }
+  },
+  {
+    'id': 'esri',
+    'name': 'ESRI Satellite Imagery',
+    'url': {
+      'version': 8,
+      'sources': {
+        'esri': {
+          'type': 'raster',
+          'tiles': [
+            'https://server.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}?blankTile=false'
+          ],
+          'tileSize': 256,
+          'attribution': 'ESRI World Imagery',
+          'maxzoom': 14
+        }
+      },
+      'layers': [
+        {
+          'id': 'esri',
+          'type': 'raster',
+          'source': 'esri'
+        }
+      ],
+      "sprite": "https://openmaptiles.github.io/positron-gl-style/sprite",
+      "glyphs": "https://cdn.ons.gov.uk/maptiles/osm/fontsv1/{fontstack}/{range}.pbf",
+    }
+  },
+  {
+    'id': 'openstreetmap',
+    'name': 'OpenStreetMap',
+    'url': {
+      'version': 8,
+      'sources': {
+        'osm-tiles': {
+          'type': 'raster',
+          'tiles': [
+            'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          ],
+          'tileSize': 256,
+          'attribution': '<a href="https://openstreetmap.org" target="_blank">OpenStreetMap</a>',
+          'maxzoom': 17
+        }
+      },
+      'layers': [
+        {
+          'id': 'osm-tiles',
+          'type': 'raster',
+          'source': 'osm-tiles'
+        }
+      ],
+      "sprite": "https://openmaptiles.github.io/positron-gl-style/sprite",
+      "glyphs": "https://cdn.ons.gov.uk/maptiles/osm/fontsv1/{fontstack}/{range}.pbf",
+    }
+  }
+];
+const overlays = [
+  {
+    'id': 'composite',
+    'name': 'OS Roads 2020 (overlay)',
+    'url': 'https://api.maptiler.com/tiles/uk-openzoomstack/tiles.json?key=itRIjT2UWTbpcSI8BSvg',
+    'layers': overLayers
+  }
+];
